fdf84254330caa780f65da5ef58e9e4026341ebc
fkadeal <44930374+fkadeal@users.noreply.github.com>
2020-10-30 05:47:59 -0700

    Updates

---
+++ precache-manifest.408348e444f8ac01f3f20a9810c4f92b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4f443c8a472ae3b10930aae4cb140b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2adcc190270498d22b3a",
+    "url": "/static/css/0.9b2f7aac.chunk.css"
+  },
+  {
+    "revision": "fdd50133eadb9c588ba6",
+    "url": "/static/css/3.387c1cdd.chunk.css"
+  },
+  {
+    "revision": "547be4e070c6649644ef",
+    "url": "/static/css/5.6ede3073.chunk.css"
+  },
+  {
+    "revision": "d869219cd6101dcf42a6",
+    "url": "/static/css/7.06f67155.chunk.css"
+  },
+  {
+    "revision": "ea5182a2c767112b0ec6",
+    "url": "/static/css/main.876a12f1.chunk.css"
+  },
+  {
+    "revision": "2adcc190270498d22b3a",
+    "url": "/static/js/0.c0863dfe.chunk.js"
+  },
+  {
+    "revision": "fdd50133eadb9c588ba6",
+    "url": "/static/js/3.51231293.chunk.js"
+  },
+  {
+    "revision": "a026f2c0e0e7d9e1db16",
+    "url": "/static/js/4.b6223aff.chunk.js"
+  },
+  {
+    "revision": "547be4e070c6649644ef",
+    "url": "/static/js/5.1288de99.chunk.js"
+  },
+  {
+    "revision": "9274039d8158c71b4ffb",
+    "url": "/static/js/6.685d466a.chunk.js"
+  },
+  {
+    "revision": "d869219cd6101dcf42a6",
+    "url": "/static/js/7.89276954.chunk.js"
+  },
+  {
+    "revision": "ea5182a2c767112b0ec6",
+    "url": "/static/js/main.14d2a5a8.chunk.js"
+  },
+  {
+    "revision": "9c95f1f71dbb56959a7b",
+    "url": "/static/js/runtime~main.68955966.js"
+  },
+  {
+    "revision": "1e4dc48872b0f8184bad22e809a3f7cc",
+    "url": "/static/media/bubble-tail-left.1e4dc488.svg"
+  },
+  {
+    "revision": "6b94009ce33b78259659e12326de498f",
+    "url": "/static/media/bubble-tail-right.6b94009c.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "fb38f7d6fbf79f207edfa158c10f16a7",
+    "url": "/static/media/check.fb38f7d6.svg"
+  },
+  {
+    "revision": "8e3f975b0dac0d7ddc9f5ab85f24d15a",
+    "url": "/static/media/telegram-logo.8e3f975b.svg"
+  }
+]);
